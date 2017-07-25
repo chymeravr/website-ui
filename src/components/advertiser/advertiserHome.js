e64b4e708282650bff3d7595d730adadc254a1df
sushilmiitb <sushil@chymeravr.com>
2017-07-25 18:56:06 +0530

    Added product/services and benefits section to advertiser page

--- src/components/advertiser/advertiserHome.js
+++ src/components/advertiser/advertiserHome.js
@@ -4,6 +4,8 @@ import { debug, callApiWithJwt } from '../../lib.js'
 import { SignUp } from './../signUpRow'
 import { Grid, Form, Button, Header, Input, Icon, Image, Message, Divider, Segment, Container, List } from 'semantic-ui-react'
 import {OneColumnSection} from '../websiteComponents/OneColumnSection';
+import {ImageTextSection} from '../websiteComponents/ImageTextSection';
+import {ScrollingIcons} from '../websiteComponents/ScrollingIcons';
 
 export class AdvertiserHomeView extends React.Component {
     constructor(props) {
@@ -73,109 +75,116 @@ export class AdvertiserHomeView extends React.Component {
                             }
                         }
                     />
-                    <Grid.Row columns={3} verticalAlign='middle' className='section'>
-                        <Grid.Column only='computer' width={2} />
-                        <Grid.Column only='computer' width={4}>
-                            <Image centered src="/static/img/access-to-premium.png" width='200px' />
+                    <ImageTextSection
+                        image='/static/img/advertiser/high-value-audience.png'
+                        leftAligned
+                        reversed
+                        content={
+                            {
+                                header:"High value audience",
+                                para:"Video ads in mobile and web, have been historically skipped by around 80% of users. We did our first 360 video ad campaign with Audi. \
+                                Both us and our client, were surprised to see the result. Even without expending justifiable resources in ad creation, the campaign was able to \
+                                acheive a 53% of skip rate. This campaign was run on high end VR devices - HTC Vive and Oculus Rift."
+                            }
+                        }
+                    />
+                    <ImageTextSection
+                        image='/static/img/advertiser/globe.gif'
+                        rightAligned
+                        content={
+                            {
+                                header:"Global reach",
+                                para:"Video ads in mobile and web, have been historically skipped by around 80% of users. We did our first 360 video ad campaign with Audi. \
+                                Both us and our client, were surprised to see the result. Even without expending justifiable resources in ad creation, the campaign was able to \
+                                acheive a 53% of skip rate. This campaign was run on high end VR devices - HTC Vive and Oculus Rift."
+                            }
+                        }
+                    />
+                    <ImageTextSection
+                        image='/static/img/advertiser/brand-recall.png'
+                        leftAligned
+                        reversed
+                        content={
+                            {
+                                header:"Higher brand recall",
+                                para:"Video ads in mobile and web, have been historically skipped by around 80% of users. We did our first 360 video ad campaign with Audi. \
+                                Both us and our client, were surprised to see the result. Even without expending justifiable resources in ad creation, the campaign was able to \
+                                acheive a 53% of skip rate. This campaign was run on high end VR devices - HTC Vive and Oculus Rift."
+                            }
+                        }
+                    />
+                    <OneColumnSection className="section"
+                        icon = {
+                            {
+                                image: "/static/img/advertiser/offering.png",
+                                caption: "Offerings"
+                            }
+                        }
+                        content = {
+                            {
+                                header: "Range of product and services",
+                                para: "Chymera VR is an advertisement platform for Virtual Reality content. We provide a host of solutions to both VR publishers and advertisers."
+                            }
+                        }
+                    />
+                    <ImageTextSection
+                        image='/static/img/advertiser/image360.jpg'
+                        leftAligned
+                        reversed
+                        content={
+                            {
+                                header:"360 ads",
+                                para:"Video ads in mobile and web, have been historically skipped by around 80% of users. We did our first 360 video ad campaign with Audi. \
+                                Both us and our client, were surprised to see the result. Even without expending justifiable resources in ad creation, the campaign was able to \
+                                acheive a 53% of skip rate. This campaign was run on high end VR devices - HTC Vive and Oculus Rift."
+                            }
+                        }
+                    />
+                    <ImageTextSection
+                        image='/static/img/advertiser/native-ads.jpg'
+                        rightAligned
+                        content={
+                            {
+                                header:"Native ads",
+                                para:"Video ads in mobile and web, have been historically skipped by around 80% of users. We did our first 360 video ad campaign with Audi. \
+                                Both us and our client, were surprised to see the result. Even without expending justifiable resources in ad creation, the campaign was able to \
+                                acheive a 53% of skip rate. This campaign was run on high end VR devices - HTC Vive and Oculus Rift."
+                            }
+                        }
+                    />
+                    <ImageTextSection
+                        image='/static/img/advertiser/analytics.png'
+                        leftAligned
+                        reversed
+                        content={
+                            {
+                                header:"Behavioral analytics",
+                                para:"Video ads in mobile and web, have been historically skipped by around 80% of users. We did our first 360 video ad campaign with Audi. \
+                                Both us and our client, were surprised to see the result. Even without expending justifiable resources in ad creation, the campaign was able to \
+                                acheive a 53% of skip rate. This campaign was run on high end VR devices - HTC Vive and Oculus Rift."
+                            }
+                        }
+                    />
+                    <ImageTextSection
+                        image='/static/img/advertiser/vr-creation.jpg'
+                        rightAligned
+                        content={
+                            {
+                                header:"Content creation service",
+                                para:"Video ads in mobile and web, have been historically skipped by around 80% of users. We did our first 360 video ad campaign with Audi. \
+                                Both us and our client, were surprised to see the result. Even without expending justifiable resources in ad creation, the campaign was able to \
+                                acheive a 53% of skip rate. This campaign was run on high end VR devices - HTC Vive and Oculus Rift."
+                            }
+                        }
+                    />
+                    <Grid.Row></Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={3}>
+                            <Header as="h3" className="centerText">Proud to work with</Header>
                         </Grid.Column>
-                        <Grid.Column width={6}>
-                            <Header as='h1' className='boldText blue'>
-                                Looking beyond mobile ads
-                            </Header>
-                            <p style={{ lineHeight: '35px' }}>
-                                Chymera ads served have generated <b> 60% higher </b> click-through rates (<b>CTR</b>) and <b>200% higher CPMs </b> compared to 2D mobile ads.
-                                <br /> <br />VR headsets capture data that hasn’t been captured before and we analyze this novel data to generate User Behavior Cohorts and
-                                Engagement reports
-                            </p>
-                        </Grid.Column>
-                        <Grid.Column width={2} />
                     </Grid.Row>
-                    <Grid.Row columns={3} verticalAlign='middle' className='section greyBackground'>
-                        <Grid.Column only='computer' width={2} />
-                        <Grid.Column width={6}>
-                            <Header as='h1' className='boldText blue'>
-                                Global audience reach
-                            </Header>
-                            <p style={{ lineHeight: '35px' }}>
-                                <ul>
-                                    <li>Publishers in US, UK and France</li>
-                                    <li>Ads served to users from 40+ countries</li>
-                                    <li>Advertisers from 10+ countries</li>
-                                </ul>
-                            </p>
-                        </Grid.Column>
-                        <Grid.Column only='computer' width={4}>
-                            <Image centered src="/static/img/global-audience.png" width='200px' />
-                        </Grid.Column>
-                        <Grid.Column width={2} />
-                    </Grid.Row>
-                    <Grid.Row columns={3} verticalAlign='middle' className='section'>
-                        <Grid.Column only='computer' width={2} />
-                        <Grid.Column only='computer' width={4}>
-                            <Image centered src="/static/img/analytics-to-understand.png" width='300px' />
-                        </Grid.Column>
-                        <Grid.Column width={6}>
-                            <Header as='h1' className='boldText blue'>
-                                Analytics
-                            </Header>
-                            <p style={{ lineHeight: '35px' }}>
-                                <ul>
-                                    <li>Reports to analyze how well did your ads perform</li>
-                                    <li>Analytics to help you improve ad creatives</li>
-                                    <li>Reports to understand how user behaved in your ad scene</li>
-                                </ul>
-                            </p>
-                        </Grid.Column>
-                        <Grid.Column width={2} />
-                    </Grid.Row>
-                    <Grid.Row columns={3} verticalAlign='middle' className='section greyBackground'>
-                        <Grid.Column only='computer' width={2} />
-                        <Grid.Column width={6}>
-                            <Header as='h1' className='boldText blue'>
-                                Ad creation services
-                            </Header>
-                            <p style={{ lineHeight: '35px' }}>
-                                Content creation in VR is non-trivial and a badly designed ad can put off users, thereby lowering your brand value.
-                                Chymera pays special attention to user experience leading to much more engagement and a higher ROI.
-                                <br /><br />
-                                Join our network and get access to our <b> Partner VR content creators</b> who have designed award winning ads.
-                            </p>
-                        </Grid.Column>
-                        <Grid.Column only='computer' width={4}>
-                            <Image centered src="/static/img/ad-creation-services.png" width='200px' />
-                        </Grid.Column>
-                        <Grid.Column width={2} />
-                    </Grid.Row>
-                    <Grid.Row columns={2} className="greyGradientBackground section" style={{ paddingBottom: '0px' }}>
-                        <Grid.Column>
-                            <Grid centered stackable>
-                                <Grid.Row columns={1}>
-                                    <Grid.Column>
-                                        <Header as='h2' className='greyText centerText'>Ad Formats</Header>
-                                        <Image centered src="/static/img/lines-heading.png" />
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row columns={2}>
-                                    <Grid.Column verticalAlign='top' width={8}>
-                                        <Image centered src="/static/img/360-image-video.png" />
-                                        <Header as='h3' className="boldText blue"> 360 IMAGE/VIDEO ADS</Header>
-                                        <p style={{ paddingLeft: '40px', paddingRight: '40px' }}>Fully immersive user experience using 360 Ad creatives</p>
-                                    </Grid.Column>
-                                    <Grid.Column verticalAlign='top' width={8}>
-                                        <Image centered src="/static/img/2d-texture.png" />
-                                        <Header as='h3' className="boldText blue">TEXTURE ADS</Header>
-                                        <p style={{ paddingLeft: '40px', paddingRight: '40px' }}>Traditional 2D Ads, strategically placed within VR environment</p>
-                                    </Grid.Column>
-                                </Grid.Row>
-                                <Grid.Row only='computer' />
-                                <Grid.Row>
-                                    <Grid.Column>
-                                        <Image size='medium' centered src="/static/img/all-platforms.png" />
-                                    </Grid.Column>
-                                </Grid.Row>
-                            </Grid>
-                        </Grid.Column>
-                    </Grid.Row>
+                    <ScrollingIcons visibleElements="four" items={11} folder="/static/img/customerLogos" />
+                    <Grid.Row></Grid.Row>
                     <Grid.Row columns={16} verticalAlign='middle' className="section">
                         <SignUp headingRow={signUpHeader} />
                     </Grid.Row>
